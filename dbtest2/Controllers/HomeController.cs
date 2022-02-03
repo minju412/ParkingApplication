7335ecb99ae29b78ca88062a1a00f72617bdd4b0
minju412 <ln8847@ajou.ac.kr>
2022-02-04 08:56:12 +0900

    initial

---
+++ dbtest2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Logging;
+//using Oracle.ManagedDataAccess.Client;
+//using ParkingLot.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Data;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbtest2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ dbtest2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dbtest2.MvcApplication" Language="C#" %>
