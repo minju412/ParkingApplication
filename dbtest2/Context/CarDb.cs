c9713e760885fdbf5ca52acea387a8be8dde9079
minju412 <ln8847@ajou.ac.kr>
2022-02-04 15:07:11 +0900

    [#2] db 연결
    - 입차 하기 탭을 눌렀을 때, 입차 페이지로 넘어감
    
    - 차량 정보를 입력하는 페이지가 나오지만, 아직은 입력받은 정보를
      넘기지는 못함
    
    - 탭을 누름과 동시에 코드상에 있는 INSERT... SQL문을 통해 db에 데이터 넘김
    
    todo: 차량 정보를 입력받아서 db에 넘기기

---
+++ dbtest2/Context/CarDb.cs
@@ -0,0 +1,16 @@
+﻿using dbtest2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace dbtest2.Context
+{
+    public class CarDb : DbContext
+    {
+        public CarDb() : base("name=DBCS") { } //Web.config에 정의
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
---
+++ dbtest2/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace dbtest2.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Car_U { get; set; }
+
+        [DisplayName("차번호")]
+        [Required(ErrorMessage = "차량 번호를 입력하세요.")]
+        public string CarNum { get; set; }
+
+        [DisplayName("입차시각")]
+        public string InTime { get; set; }
+
+        [DisplayName("출차시각")]
+        public string OutTime { get; set; }
+    }
+}
