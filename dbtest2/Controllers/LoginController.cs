67fa8334a9272c6079ffba072687c6b20a46c3c0
minju412 <ln8847@ajou.ac.kr>
2022-02-09 11:14:21 +0900

    [#5] 로그인 페이지 생성
    - 레이아웃만 완성

---
+++ dbtest2/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using dbtest2.Context;
+using dbtest2.Models;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+using System.Web.Mvc;
+
+
+namespace dbtest2.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return Redirect("/login/login");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
