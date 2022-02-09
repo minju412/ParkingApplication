0f47e1532ec9aeca25ba7bc713010e7d14a54df9
minju412 <ln8847@ajou.ac.kr>
2022-02-09 17:05:40 +0900

    [#5] 회원가입 기능
    - Register 구현

--- dbtest2/Controllers/LoginController.cs
+++ dbtest2/Controllers/LoginController.cs
@@ -1,7 +1,13 @@
 ﻿using dbtest2.Context;
 using dbtest2.Models;
+using dbtest2.Models.Login;
+using dbtest2.ViewModel;
 using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Data;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
 using System.Web.Mvc;
 
 
@@ -10,20 +16,142 @@ namespace dbtest2.Controllers
     public class LoginController : Controller
     {
 
+        // [개발토끼]
+
+        //[HttpGet] //디폴트가 GET 방식
+        //public ActionResult Login()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult Login(LoginViewModel model)
+        //{
+        //    // ID, 비밀번호 - 필수
+        //    //if (ModelState.IsValid)
+        //    //{
+        //    //    using (var db = new CarDb())
+        //    //    {
+        //    //        // Linq - 메서드 체이닝
+        //    //        var user = db.Users
+        //    //            .FirstOrDefault(u => u.Email.Equals(model.Email) &&
+        //    //                            u.Password.Equals(model.Password));
+
+        //    //        //로그인에 성공했을 때
+        //    //        if (user != null)
+        //    //        {
+        //    //            // 세션에 로그인 정보 담음
+        //    //            HttpContext.Session.SetInt32("USER_LOGIN_KEY", user.User_Seq);
+
+        //    //            return RedirectToAction("LoginSuccess", "Home");
+        //    //        }
+        //    //    }
+        //    //    //로그인에 실패했을 때
+        //    //    ModelState.AddModelError(string.Empty, "사용자 ID 혹은 비밀번호가 올바르지 않습니다.");
+        //    //}
+        //    return View(model);
+        //}
+
+        //public ActionResult Logout()
+        //{
+        //    HttpContext.Session.Remove("USER_LOGIN_KEY");
+
+        //    return RedirectToAction("Index", "Home");
+        //}
+
+        //public ActionResult Register()
+        //{
+        //    return View();
+        //}
+
+
+        /////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////
+
+
+        // [bluepope]
+
         public ActionResult Index()
         {
             return Redirect("/login/login");
         }
 
+        [HttpGet] //디폴트가 GET 방식
         public ActionResult Login()
         {
             return View();
         }
 
-        public ActionResult Register()
+        [HttpPost]
+        [Route("/login/login")]
+        //public ActionResult LoginProc(LoginViewModel input)
+        public ActionResult LoginProc(UserModel input) //fromform
         {
+            //try
+            //{
+            //    input.ConvertPassword(); //비밀번호 암호화
+            //    var user = input.GetLoginUser();
+
+            //    // 로그인 작업
+
+            //    var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+            //    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.User_Seq.ToString()));
+            //    identity.AddClaim(new Claim(ClaimTypes.Name, user.User_Name));
+            //    identity.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+            //    identity.AddClaim(new Claim("LastCheckDateTime", DateTime.UtcNow.ToString("yyyyMMddHHmmss")));
+
+            //    var principal = new ClaimsPrincipal(identity);
+
+            //    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties
+            //    {
+            //        IsPersistent = false,
+            //        ExpiresUtc = DateTime.UtcNow.AddHours(4),
+            //        AllowRefresh = true
+            //    });
+
+            //    return Redirect("/");
+            //}
+            //catch (Exception ex)
+            //{
+            //    return Redirect($"/login/login?msg={HttpUtility.UrlEncode(ex.Message)}");
+            //}
+
             return View();
         }
 
+        public ActionResult Register(string msg)
+        {
+            ViewData["msg"] = msg;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserModel input)
+        {
+            try
+            {
+                string password2 = Request.Form["password2"];
+
+                if (input.Password != password2)
+                {
+                    throw new Exception("password와 password2가 다릅니다");
+                }
+
+                // 비밀번호 암호화
+                input.ConvertPassword();
+
+                // db에 insert
+                input.Register();
+
+                // 회원가입 성공
+                return Redirect("/login/login");
+            }
+            catch (Exception ex)
+            {
+                // 실패
+                return Redirect($"/login/register?msg={HttpUtility.UrlEncode(ex.Message)}");
+            }
+        }
+       
     }
 }
---
+++ dbtest2/Models/UserModel.cs
@@ -0,0 +1,49 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace dbtest2.Models.Login
+{
+    public class UserModel
+    {
+        //[Key] // PK 설정
+        public int User_Seq { get; set; }
+
+        //[Required(ErrorMessage = "사용자 이름을 입력하세요.")] // Not Null 설정
+        public string User_Name { get; set; }
+
+        //[Required(ErrorMessage = "사용자 이메일을 입력하세요.")] 
+        public string Email { get; set; }
+
+        //[Required(ErrorMessage = "사용자 비밀번호를 입력하세요.")] 
+        public string Password { get; set; }
+
+        public void ConvertPassword() // password 암호화
+        {
+            var sha = new System.Security.Cryptography.HMACSHA512();
+            sha.Key = System.Text.Encoding.UTF8.GetBytes(this.Password.Length.ToString());
+
+            // 암호화된 해쉬값
+            var hash = sha.ComputeHash(System.Text.Encoding.UTF8.GetBytes(this.Password));
+
+            this.Password = System.Convert.ToBase64String(hash);
+        }
+
+        internal int Register()
+        {
+            string sql = "INSERT INTO c_user (user_seq,user_name,email,password) VALUES (C_USER_SEQ.NEXTVAL,:user_name,:email,:password)";
+
+            string _strConn = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xe)));User Id=ann;Password=111111;";
+
+            using (var conn = new OracleConnection(_strConn))
+            {
+                conn.Open();
+
+                return Dapper.SqlMapper.Execute(conn, sql, this);
+            }
+        }
+    }
+}
