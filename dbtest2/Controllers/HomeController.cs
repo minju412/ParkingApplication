6cb90b1513c2004932001b7d18f4daf01f51ef23
minju412 <ln8847@ajou.ac.kr>
2022-02-10 17:15:56 +0900

    [#2] 입차하기 페이지 입력 폼
    - TableInsert 입력 폼 완성
    
    - todo: 입력 받아서 db에 넣기

--- dbtest2/Controllers/HomeController.cs
+++ dbtest2/Controllers/HomeController.cs
@@ -21,14 +21,14 @@ namespace dbtest2.Controllers
             return View(Car.GetList(search));
         }
 
-        [Authorize]
-        public ActionResult TableWrite()
+        //[Authorize] // 로그인 해야 입차 가능
+        public ActionResult TableInsert()
         {
             return View();
         }
     
         [Authorize]
-        public ActionResult TableWrite_Input(string carnum, DateTime intime)
+        public ActionResult TableInsert_Input(string carnum, DateTime intime)
         {
             var model = new Car();
 
@@ -42,71 +42,6 @@ namespace dbtest2.Controllers
             return Redirect("/home/tablelist");
         }
 
-        //public ActionResult TableList()
-        //{
-        //    var dt = new DataTable();
-
-        //    string _strConn = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xe)));User Id=ann;Password=111111;";
-
-
-        //    // 오라클 연결
-        //    using (var conn = new OracleConnection(_strConn))
-        //    {
-        //        // 연결
-        //        conn.Open();
-
-        //        // 명령 객체 생성
-        //        //OracleCommand cmd = new OracleCommand();
-        //        using (var cmd = new OracleCommand())
-        //        {
-        //            string carnum = "1234";
-
-        //            cmd.Connection = conn;
-
-        //            // 파라미터 바인딩
-        //            cmd.CommandText = "SELECT * FROM c_table ORDER BY car_id ASC";
-
-        //            cmd.Parameters.Add(new OracleParameter("@carnum", carnum));
-
-        //            // 결과 리더 객체를 리턴
-        //            var reader = cmd.ExecuteReader();
-
-        //            dt.Load(reader);
-        //        }
-        //    }
-        //    ViewData["dt"] = dt;
-
-        //    return View();
-        //}
-
-
-        //public ActionResult Create()
-        //{
-        //    //var dt = new DataTable();
-        //    string _strConn = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xe)));User Id=ann;Password=111111;";
-        //    using (var conn = new OracleConnection(_strConn))
-        //    {
-        //        conn.Open();
-
-        //        using (var cmd = new OracleCommand())
-        //        {
-        //            cmd.Connection = conn;
-
-        //            cmd.CommandText = "INSERT INTO c_table VALUES (1,'1333','10:00','16:00')";
-        //            //cmd.CommandText = "INSERT INTO c_table (car_id,carnum,intime,outtime) VALUES (C_TABLE_SEQ.NEXTVAL,:carnum,:intime,:outtime)";
-
-        //            cmd.ExecuteNonQuery();
-
-        //            //cmd.Parameters.Add(new OracleParameter("@carnum", carnum));
-        //            //var reader = cmd.ExecuteReader();
-        //            //dt.Load(reader);
-        //        }
-        //    }
-        //    //ViewData["dt"] = dt;
-
-        //    //return Json(new { msg = "OK" }, JsonRequestBehavior.AllowGet);
-        //    return View();
-        //}
 
     }
 }
